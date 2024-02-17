584cef89013a4386c5ea737051498d40944596b2
Richard Decal <public@richarddecal.com>
2024-02-16 20:24:11 -0500

    Bump version: 0.1.1 → 0.1.2

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ try:
     version = release = get_distribution("precision_recall_gain").version
 except Exception:
     traceback.print_exc()
-    version = release = "0.1.1"
+    version = release = "0.1.2"
 
 pygments_style = "trac"
 templates_path = ["."]
